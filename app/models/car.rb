b65a091abbc485c1dced983718b9f5a1ab9df79e
Alnoor Pirani <alnoorp@gmail.com>
2014-01-03 00:49:48 -0500

    add error checking

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,7 @@
 class Car < ActiveRecord::Base
+  validates_presence_of :color
+  validates_presence_of :year
+  validates_presence_of :mileage
   validates :year, presence: true,
     numericality: { only_integer: true, greater_than_or_equal_to: 1980 }
 end
--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Car do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:blanks) { ['', nil] }
+
+  it { should_not have_valid(:color).when(*blanks) }
+  it { should have_valid(:color).when('green') }
+
+  it { should_not have_valid(:year).when(1979, *blanks) }
+  it { should have_valid(:year).when(2012) }
+
+  it { should_not have_valid(:mileage).when(*blanks) }
+  it { should have_valid(:mileage).when(50000) }
+
+  it { should have_valid(:description).when(*blanks) }
+  it { should have_valid(:description).when('This is more than a green car') }
+
 end
